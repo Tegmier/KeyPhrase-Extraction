54359e581832c675a4ec7070d8f607a2e1a47671
Tegmier <xdingming@outlook.com>
2024-05-17 12:59:05 +0900

    modification

--- models/model.py
+++ models/model.py
@@ -5,26 +5,43 @@ import tensorflow as tf
 class Model(object):
 
     def __init__(self,
+                 # 第一层神经元的数量
                  nh1,
+                 # 第二层神经元的数量
                  nh2,
+                 # 待定
                  ny,
+                 # 待定
                  nz,
+
                  de,
+
                  cs,
+                 # 学习率 
                  lr,
+                 #学习率衰减因子
                  lr_decay,
+                 
                  embedding,
+
                  max_gradient_norm,
                  model_cell='rnn',
                  model='basic_model',
                  nonstatic=False):
-
+        
+        # placeholder定义
+        # 定义batch大小（一个batch代表单次训练的句子数量）
         self.batch_size = tf.placeholder(dtype=tf.int32, shape=None)
+        # 定义了输入input_x，形状是None*None*cs
         self.input_x=tf.placeholder(tf.int32,shape=[None,None,cs],name='input_x')
+        # 定义了输入input_y，形状是None*None
         self.input_y=tf.placeholder(tf.int32,shape=[None,None],name="input_y")
+        # 定义了输入input_z，形状是None*None
         self.input_z=tf.placeholder(tf.int32,shape=[None,None],name='input_z')
+        # 定义了dropout参数（随机舍弃一部分神经元）
         self.keep_prob=tf.placeholder(dtype=tf.float32,name='keep_prob')
-
+        
+        # 定义了学习率lr
         self.lr=tf.Variable(lr,dtype=tf.float32)
 
         self.learning_rate_decay_op = self.lr.assign(
@@ -33,17 +50,23 @@ class Model(object):
 
         #Creating embedding input
         with tf.device("/cpu:0"),tf.name_scope('embedding'):
+            # 判断是否使用静态嵌入
             if nonstatic:
                 W=tf.constant(embedding,name='embW',dtype=tf.float32)
+            # 否则使用动态嵌入
             else:
                 W=tf.Variable(embedding,name='embW',dtype=tf.float32)
+            # 通过lookup函数重新把输入向量转化为embeddings
             inputs=tf.nn.embedding_lookup(W,self.input_x)
+            # 通过reshape函数把embedding张量重新塑性
             inputs=tf.reshape(inputs,[self.batch_size,-1,cs*de])
 
-        #Droupout embedding input
+        #Dropout embedding input
+        # 通过Dropout的方式干掉部分输入
         inputs=tf.nn.dropout(inputs,keep_prob=self.keep_prob,name='drop_inputs')
 
         #Create the internal multi-layer cell for rnn
+        # 创建神经元
         if model_cell=='rnn':
             single_cell1=tf.nn.rnn_cell.BasicRNNCell(nh1)
             single_cell2=tf.nn.rnn_cell.BasicRNNCell(nh2)
@@ -55,13 +78,18 @@ class Model(object):
             single_cell2=tf.nn.rnn_cell.GRUCell(nh2)
         else:
             raise 'model_cell error!'
+        
         #DropoutWrapper rnn_cell
+        # Dropout随机干掉神经元
         single_cell1 = tf.nn.rnn_cell.DropoutWrapper(single_cell1, output_keep_prob=self.keep_prob)
         single_cell2 = tf.nn.rnn_cell.DropoutWrapper(single_cell2, output_keep_prob=self.keep_prob)
-      
-        self.init_state=single_cell1.zero_state(self.batch_size,dtype=tf.float32) 
         
+        # 创建RNN神经元初始状态
+        self.init_state=single_cell1.zero_state(self.batch_size,dtype=tf.float32) 
+        self.init_state=single_cell2.zero_state(self.batch_size,dtype=tf.float32)
+
         #RNN1
+        # 把第一层给定义好了
         with tf.variable_scope('rnn1'):
             self.outputs1,self.state1=tf.nn.dynamic_rnn(
                 cell=single_cell1,
@@ -71,6 +99,7 @@ class Model(object):
             )
 
         #RNN2
+        # 把第二层给定义好了
         with tf.variable_scope('rnn2'):
             self.outputs2,self.state2=tf.nn.dynamic_rnn(
                 cell=single_cell2,
